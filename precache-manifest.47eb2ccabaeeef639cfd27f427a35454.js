0f04c796053d6c0b5dfd1d3cff3d36afca5beef2
stefanghadjiew <stefanghadjiew@abv.bg>
2020-09-29 11:33:24 +0300

    Updates

---
+++ precache-manifest.47eb2ccabaeeef639cfd27f427a35454.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "952b65af1257cad56669b269712b6a9a",
+    "url": "/chat-app-static/index.html"
+  },
+  {
+    "revision": "a14867419d3f29d53c63",
+    "url": "/chat-app-static/static/css/main.043c3a67.chunk.css"
+  },
+  {
+    "revision": "67d43a9246dc6c80af9c",
+    "url": "/chat-app-static/static/js/2.f84ba0f3.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/chat-app-static/static/js/2.f84ba0f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a14867419d3f29d53c63",
+    "url": "/chat-app-static/static/js/main.56636bd3.chunk.js"
+  },
+  {
+    "revision": "37a258733a6605cc75bf",
+    "url": "/chat-app-static/static/js/runtime-main.74fcc6ba.js"
+  },
+  {
+    "revision": "718cb85330193b2b2549286119915279",
+    "url": "/chat-app-static/static/media/wallhaven-01py33.718cb853.jpg"
+  }
+]);
